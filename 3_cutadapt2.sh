b739d386dc88534306133de02ab0b5fd8fe1f1b0
Kristen N. Finch <finchkn@uw.edu>
2021-11-29 10:16:25 -0800

    email redacted

--- 3_cutadapt2.sh
+++ 3_cutadapt2.sh
@@ -7,8 +7,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=8G
 #SBATCH --time=48:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=khan3@uw.edu
 #SBATCH -o logfiles/cutadapt.%j.out
 #SBATCH -e logfiles/cutadapt.%j.err
 
