4df4e3f777e669a71f58fa73be60bc9d509ec78f
Kristen N. Finch <finchkn@uw.edu>
2021-11-29 08:58:25 -0800

    irrelevant code cleaned up

--- gnomixdb_mapmaker.sh
+++ gnomixdb_mapmaker.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-#run
-#20211015
-
 #SBATCH --job-name=sample_maps
 #SBATCH --account=wasser
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=40G
-#SBATCH --time=20:00:00
+#SBATCH --mem=1G
+#SBATCH --time=4:00:00
 #SBATCH -o sample_maps.%j.out
 #SBATCH -e sample_maps.%j.err
-
-
-
 
 module load singularity
 
 chrs=$(cat /gscratch/wasser/khan3/elephant_snps/chromosomes.txt)
-
-#dirs=$(ls -1 --hide=archive /gscratch/wasser/khan3/elephant_snps/data/gatk/gvcf/)
-
-#mkdir /gscratch/scrubbed/finchkn/tmp
-
-#for dir in $dirs
-#do
-#	cd /gscratch/wasser/khan3/elephant_snps/data/gatk/gvcf/
-#	cp $dir/*.vcf.gz /gscratch/scrubbed/finchkn/tmp/
-#done
 
 for chr in $chrs
 do
@@ -39,15 +23,3 @@ do
 		echo -e "${sampleID}\t${i}">>/gscratch/wasser/khan3/elephant_snps/data/gatk/gvcf/${chr}.map
 	done
 done
-
-#for chr in $chrs
-#do
-#	files=$(ls -1 /gscratch/scrubbed/finchkn/tmp/*vcf.gz|grep "${chr}_")
-#	for j in $files
-#	do
-#		sampleID=$(singularity exec --bind /gscratch/ /gscratch/wasser/containers/bcftools.sif bcftools query -l $j)
-#		echo -e "${sampleID}\t${j}">/gscratch/wasser/khan3/elephant_snps/data/gatk/gvcf/${chr}.map
-#	done
-#done
-
-
