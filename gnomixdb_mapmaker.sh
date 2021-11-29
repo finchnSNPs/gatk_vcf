1e4835fc15d8cf81539dbf7c383bf3647d3ad012
Kristen N. Finch <finchkn@uw.edu>
2021-11-29 09:00:28 -0800

    code annotated

--- gnomixdb_mapmaker.sh
+++ gnomixdb_mapmaker.sh
@@ -10,6 +10,16 @@
 #SBATCH -o sample_maps.%j.out
 #SBATCH -e sample_maps.%j.err
 
+#make a sample map file for generating the GenomicDB 
+
+#expected output $1=sample name desired $2=file path
+#EasternForest	/gscratch/scrubbed/finchkn/tmp/chrUn2114_EasternForest_paired_aln_sorted_dedup_g.vcf.gz
+#EasternSavanna	/gscratch/scrubbed/finchkn/tmp/chrUn2114_EasternSavanna_paired_aln_sorted_dedup_g.vcf.gz
+#NorthernSavanna1	/gscratch/scrubbed/finchkn/tmp/chrUn2114_NorthernSavanna1_paired_aln_sorted_dedup_g.vcf.gz
+#NorthernSavanna2	/gscratch/scrubbed/finchkn/tmp/chrUn2114_NorthernSavanna2_paired_aln_sorted_dedup_g.vcf.gz
+#SouthernSavanna	/gscratch/scrubbed/finchkn/tmp/chrUn2114_SouthernSavanna_paired_aln_sorted_dedup_g.vcf.gz
+#WesternForest	/gscratch/scrubbed/finchkn/tmp/chrUn2114_WesternForest_paired_aln_sorted_dedup_g.vcf.gz
+
 module load singularity
 
 chrs=$(cat /gscratch/wasser/khan3/elephant_snps/chromosomes.txt)
